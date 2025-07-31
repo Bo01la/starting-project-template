a784c51222f05bb8a959b2217789a17b0a63ae55
Bo01la <abdoehab30@gmail.com>
2025-07-31 08:10:40 +0300

    1st commit

---
+++ api/serverlessFunction.js
@@ -0,0 +1,8 @@
+// // api/yourFunction.js
+// // npm install dotenv    --> this helps me access .env variables when I am using the function locally on my computer
+// import 'dotenv/config';
+
+// export default async function handler(req, res) {
+//   const secretKey = process.env.SECRET_API_KEY;
+//   // do something with it...
+// }
---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-9xl text-amber-900 font-black" >hello world</h1>
+    </>
+  )
+}
+
+export default App
